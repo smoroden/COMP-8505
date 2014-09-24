04c3889ad92f284c2da740d5e8baac4db16a1ef4
smoroden <zach@smoroden.com>
2014-09-24 15:52:14 -0700

    nothing really new

--- Assignment 2/filter_test.rb
+++ Assignment 2/filter_test.rb
@@ -3,7 +3,7 @@ require 'rubygems'
 require 'pcaprub'
 require 'pp'
 
-
+# Opens up the nic for capture.
 capture = PCAPRUB::Pcap.open_live('em1', 65535, true, 0)
 capture.setfilter('tcp[2:2] = 56566')
 while 1==1
--- README.md
+++ README.md
@@ -3,4 +3,5 @@ TCP Capture will be done with 'pcaprub'
 
 
 ****** IF WE HAVE TIME *********
+---------------------------------
 find the most common command in htop and then change our process to that.
