bc447f1f23a115c3f9269e462f26265ac6077481
ThreadedLemon <slade205@gmail.com>
2014-09-29 23:59:11 -0700

    added client and data encryption. Currently compiles but does not run.

---
+++ Assignment 2/client.c
@@ -0,0 +1,150 @@
+#include "client.h"
+
+int main (int argc, char **argv)
+{
+    struct AddrInfo UserInfo;
+    struct AddrInfo *Addr_Ptr;
+    int arg, opt;
+
+    if (argc < 3)
+    {
+        usage(argv);
+    }
+
+    Addr_Ptr = &UserInfo;
+    // Set the default values
+    Addr_Ptr->SrcHost = DEFAULT_SRC_IP;		// Deafult Source IP
+    Addr_Ptr->DstHost = NULL;			    // Must be specified by user!
+    Addr_Ptr->dport = DEFAULT_DST_PORT;		// Default Destination Port
+    Addr_Ptr->sport = DEFAULT_SRC_PORT;		// Default Source Port
+    Addr_Ptr->cmd = NULL;                   // Must be specified by user!
+
+    // Process command line options
+    while ((opt = getopt (argc, argv, OPTIONS)) != -1)
+    {
+    	switch (opt)
+    	{
+    	    case 'h':
+    	      Addr_Ptr->SrcHost = optarg;
+    	    break;
+
+    	    case 'd':
+    	      Addr_Ptr->DstHost = optarg;		// Destination Host name
+    	    break;
+
+    	    case 'p':
+    	      Addr_Ptr->dport = atoi (optarg);
+    	    break;
+
+    	    case 's':
+    	      Addr_Ptr->sport = atoi (optarg);
+    	    break;
+
+            case 'c':
+                Addr_Ptr->cmd = optarg;
+            break;
+
+    	    default:
+    	      case '?':
+    	      usage (argv);
+    	    break;
+    	}
+    }
+
+    if (Addr_Ptr->DstHost == NULL || Addr_Ptr->cmd == NULL)
+    	usage (argv);
+
+    //Create a raw socket
+    Addr_Ptr->RawSocket = socket (PF_INET, SOCK_RAW, IPPROTO_UDP);
+
+    // Set SO_REUSEADDR so that the port can be resused for further invocations of the application
+    arg = 1;
+    if (setsockopt (Addr_Ptr->RawSocket, SOL_SOCKET, SO_REUSEADDR, &arg, sizeof(arg)) == -1)
+    perror("setsockopt");
+
+    return 0;
+}
+
+void send_packet(struct AddrInfo *UserAddr)
+{
+    char datagram[PKT_SIZE], *data; 	// set the Datagram (packet) size
+    struct iphdr *iph = (struct iphdr *) datagram;	 //IP header
+    struct udphdr *udph = (struct udphdr *) (datagram + sizeof (struct ip));	//UDP header
+    struct sockaddr_in sin;
+    pseudo_header psh;
+
+    sin.sin_family = AF_INET;
+    sin.sin_port = htons (UserAddr->dport);
+    sin.sin_addr.s_addr = inet_addr (UserAddr->DstHost);
+
+    memset (datagram, 0, PKT_SIZE); // zero out the buffer where the datagram will be stored
+
+    //Data part
+    data = datagram + sizeof(struct iphdr) + sizeof(struct udphdr);
+    strcpy(data , encrypt(ENCRYPTION_KEY, UserAddr->cmd));
+
+    //IP Header Fields
+    iph->ihl = 5;		// IP Header Length
+    iph->version = 4;		// Version 4
+    iph->tos = 0;
+    iph->tot_len = sizeof (struct ip) + sizeof (struct udphdr) + strlen(data);	// Calculate the total Datagram size
+    iph->id = htonl (DEFAULT_IP_ID); 	//IP Identification Field
+    iph->frag_off = 0;
+    iph->ttl = DEFAULT_TTL;		// Set the TTL value
+    iph->protocol = IPPROTO_UDP;
+    iph->check = 0;      	//Initialize to zero before calculating checksum
+    iph->saddr = inet_addr (UserAddr->SrcHost);  //Source IP address
+    iph->daddr = sin.sin_addr.s_addr;
+
+    iph->check = csum ((unsigned short *) datagram, iph->tot_len >> 1);
+
+    //UDP Header Fields
+    udph->source = htons (UserAddr->sport); // UDP source port
+    udph->dest = htons (UserAddr->dport);	// UDP destination port
+    udph->len = htons(8 + strlen(UserAddr->cmd)); //UDP header size plus data (command)
+    udph->check = 0;			// Initialize the checksum to zero
+
+    // calcluate the IP checksum
+    psh.source_address = inet_addr(UserAddr->SrcHost);
+    psh.dest_address = sin.sin_addr.s_addr;
+    psh.placeholder = 0;
+    psh.protocol = IPPROTO_UDP;
+    psh.udp_length = htons(sizeof(struct udphdr) + strlen(data));
+
+    memcpy(&psh.udp , udph , sizeof (struct udphdr));
+
+    udph->check = csum( (unsigned short*) &psh , sizeof (pseudo_header));
+
+    //IP_HDRINCL to stop the kernel from building the packet headers
+    {
+        int one = 1;
+        const int *val = &one;
+        if (setsockopt (UserAddr->RawSocket, IPPROTO_IP, IP_HDRINCL, val, sizeof (one)) < 0)
+            perror ("setsockopt");
+    }
+
+    //Send the packet
+    if (sendto (UserAddr->RawSocket, datagram, iph->tot_len, 0, (struct sockaddr *) &sin, sizeof (sin)) < 0)
+    {
+    	perror ("sendto");
+    }
+}
+
+//XOR Encryption
+char* encrypt(char* key, char* string)
+{
+    int i, string_length = strlen(string);
+    for(i=0; i<string_length; i++)
+    {
+        string[i]=string[i]^key[i];
+        printf("%i", string[i]);
+    }
+    return string;
+}
+
+void usage (char **argv)
+{
+      fprintf(stderr, "Usage: %s -d <Destination IP> -p [Destination Port] -h [Source IP] -s [Source Port] -c [Command]\n", argv[0]);
+      fprintf(stderr, "You must specify the destination address and a command!\n");
+      exit(1);
+}
---
+++ Assignment 2/client.h
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <netinet/udp.h>   	// UDP Header definitions
+#include <netinet/ip.h>    	// IP Header definitions
+#include <sys/types.h>
+#include <unistd.h>
+
+#define PKT_SIZE		    4096
+#define DEFAULT_DST_PORT	8080
+#define DEFAULT_SRC_PORT	11234
+#define DEFAULT_TTL		    255
+#define DEFAULT_IP_ID		12345
+#define DEFAULT_SRC_IP		"192.168.1.5"
+#define OPTIONS 		    "?h:d:s:p:c:"
+#define ENCRYPTION_KEY      "56xfek92g"
+
+// Globals
+typedef struct    //needed for checksum calculation
+{
+    unsigned int source_address;
+    unsigned int dest_address;
+    unsigned char placeholder;
+    unsigned char protocol;
+    unsigned short udp_length;
+    struct udphdr udp;
+}pseudo_header;
+
+struct AddrInfo
+{
+    int RawSocket;
+    char *DstHost;
+    char *SrcHost;
+    int dport;
+    int sport;
+    char *cmd;
+};
+
+// Function Prototypes
+void send_packet(struct AddrInfo *UserAddr);
+char* encrypt(char *key, char *string);
+void usage (char **arg );
+unsigned short csum (unsigned short *, int);
