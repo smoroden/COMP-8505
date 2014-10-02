a26cf11616386d8a6caae513753f8cba356208d4
zach <zach@smoroden.com>
2014-10-01 17:42:28 -0700

    wont capture the packets we need.

--- Assignment 2/filter_test.rb
+++ Assignment 2/filter_test.rb
@@ -2,23 +2,48 @@
 require 'rubygems'
 require 'packetfu'
 require 'pp'
-
+                    #important website http://www.rubydoc.info/github/todb/packetfu/frames/PacketFu/Inject
 include PacketFu
+$password = "zdehjk"
+
+
+  def xor(payload)
+    output = ''
+    i = 0
+    payload.each_char { |c|
+      pass_char = $password[i]
+      xor = c.ord ^ pass_char.ord
+      output << xor
+      i+=1
+      if i == ($password.size - 1)
+        i = 0
+      end
+    }
+    return output
+  end
+
 
 iface = ARGV[0] || "em1"
+
 # Opens up the nic for capture.
 def sniff(iface)
-  cap = Capture.new(:iface => iface, :start => true)
+  cap = Capture.new(:iface => iface, :start => true, :filter => 'port 22')
   cap.stream.each do |p|
-    pkt = Packet.parse p
-    if pkt.is_ip?
-      next if pkt.ip_saddr == Utils.ifconfig(iface)[:ip_saddr]
-      packet_info = [pkt.ip_saddr, pkt.ip_daddr, pkt.size, pkt.proto.last]
-      puts "%-15s -> %-15s %-4d %s" % packet_info
-      pp pkt
-    end
+    packet = Packet.parse p
+    #pp packet
+    cmd = xor packet.payload
+    pp cmd
+    pp packet.payload
+    #value = `cmd`
+
+    pp packet.ip_dst_readable
   end
+
+
 end
 
+#cap = PacketFu::Capture.new(:start => true, :iface => iface, :promisc => true)
+#cap.show_live(:filter => 'port 22')
 
 sniff(iface)
+
