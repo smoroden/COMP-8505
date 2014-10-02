52c23642f79a7a1b7b0ab266117e59aaadfcb838
ThreadedLemon <slade205@gmail.com>
2014-10-01 17:42:24 -0700

    -added packet capturing -added setup script

--- Assignment 2/client.h
+++ Assignment 2/client.h
@@ -1,20 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <pcap/pcap.h>
 #include <arpa/inet.h>
+#include <sys/socket.h>
 #include <netinet/udp.h>   	// UDP Header definitions
 #include <netinet/ip.h>    	// IP Header definitions
+#include <netinet/in.h>
+#include <netinet/if_ether.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <errno.h>
 
 #define PKT_SIZE		    4096
 #define DEFAULT_DST_PORT	8080
 #define DEFAULT_SRC_PORT	11234
 #define DEFAULT_TTL		    255
 #define DEFAULT_IP_ID		12345
-#define DEFAULT_SRC_IP		"192.168.1.5"
+#define MAXLINE			    80
+#define DEFAULT_SRC_IP		"192.168.0.5"
 #define OPTIONS 		    "?h:d:s:p:c:"
 #define ENCRYPTION_KEY      "zdehjk"
+#define FILTER              "udp and src host %s and src port %d and dst port %d"
+
 
 // Globals
 typedef struct    //needed for checksum calculation
@@ -37,8 +45,18 @@ struct AddrInfo
     char *cmd;
 };
 
+typedef struct
+{
+    pcap_t* nic_descr;
+    bpf_u_int32 netp;
+    char cmd[MAXLINE];
+}PcapInfo;
+
 // Function Prototypes
 void send_packet(struct AddrInfo *UserAddr);
+void receive_packet(PcapInfo *pcap_arg);
+void pkt_callback (u_char *ptr_null, const struct pcap_pkthdr* pkthdr, const u_char *packet);
 char* encrypt(char *key, char *string);
 void usage (char **arg );
 unsigned short csum (unsigned short *, int);
+
