97315b8b1221a5c7917676914b9fccbe21373363
ThreadedLemon <slade205@gmail.com>
2014-11-29 21:04:15 -0800

    -added comments -automatically get IP and interface

--- Final Project/test.py
+++ Final Project/test.py
@@ -1,43 +1,10 @@
-import subprocess
-import sys
-import pyinotify
+import netifaces as ni
 
-class OnWriteHandler(pyinotify.ProcessEvent):
-    def my_init(self, cwd, extension, cmd):
-        self.cwd = cwd
-        self.extensions = extension.split(',')
-        self.cmd = cmd
 
-    def _run_cmd(self):
-        print '==> Modification detected'
-        subprocess.call(self.cmd.split(' '), cwd=self.cwd)
+ip = ni.ifaddresses('em1')[2][0]['addr']
+print ip
 
-    def process_IN_CREATE(self, event):
-        if all(not event.pathname.endswith(ext) for ext in self.extensions):
-            return
-        self._run_cmd()
+interface_list = ni.interfaces()
 
-def auto_transmit(path, extension, cmd):
-    wm = pyinotify.WatchManager()
-    handler = OnWriteHandler(cwd=path, extension=extension, cmd=cmd)
-    notifier = pyinotify.Notifier(wm, default_proc_fun=handler)
-    wm.add_watch(path, pyinotify.ALL_EVENTS, rec=True, auto_add=True)
-    print '==> Start monitoring %s (type c^c to exit)' % path
-    notifier.loop()
 
-if __name__ == '__main__':
-    if len(sys.argv) < 3:
-        print >> sys.stderr, "Command line error: missing argument(s)."
-        sys.exit(1)
-
-    # Required arguments
-    path = sys.argv[1]
-    extension = sys.argv[2]
-
-    # Optional argument
-    cmd = 'make'
-    if len(sys.argv) == 4:
-        cmd = sys.argv[3]
-
-    # Blocks monitoring
-    auto_transmit(path, extension, cmd)
+print
